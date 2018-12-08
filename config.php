045fcc0faf3638e43b8d1a65bc39779e61b94cfd
jhonhansen <42970507+jhonhansen@users.noreply.github.com>
2018-12-08 23:30:35 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+$ticket = "sHp2r312otmCpnGns3mCqJhovNiBioeYr6p9sLWldtm8sHayipyu247Mfae0q3FnhHqszIudf5W7uoKktKWc2q2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ";
+$meid = "6BEE144CD23F4FB9";
+$os="android";
+$sign="5b1e6a18663261f9330b8260b197c435ed8f8c8971ef778bcf6c6cbab8fc9b6c";
