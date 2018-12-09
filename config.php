505b9f78ba218b5f1bbe393f0b7624d3b5d9dd6a
jhonhansen <42970507+jhonhansen@users.noreply.github.com>
2018-12-09 10:12:57 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-$ticket = "sHp2r312otmCpnGns3mCqJhovNiBh6Hdr9OJsbPbnc6wjYOjiYbMl4Kmcpq0m4VmkLC324ple5ewupygwKWClq2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ";
+$ticket = "sHp2r312otmCpnGns3mCqJhovNiBh57Pu9OCpMC4epeyeoelfZy6k4K2hpyzm4WskLC3mH-KkM2wuolstJOClq2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ";
 $meid = "2262e188bd16a203";
 $os="android";
-$sign="a64070ee697b8a231ab15ea81bd675bb671ab78c1926f1078af8d90549729402";
+$sign="5f76cb0960c03e0f7f8fc63718eec89b3bb857c92412d44ce5496f5be4df7b06";
